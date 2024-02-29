2cbdc9421f97b4e787ea6669ec49fd0894fcdcb4
Delfi1 <fezdx@mail.ru>
2024-02-29 20:34:27 +0300

    Update clear color;

--- src/engine/graphics/meshes.rs
+++ src/engine/graphics/meshes.rs
@@ -7,51 +7,18 @@ use glam::{DAffine3 as Transform, DVec3};
 pub struct Vertex {
     #[format(R32G32_SFLOAT)]
     position: [f32; 3],
-    #[format(R32G32_SFLOAT)]
-    normal: [f32; 3]
 }
 
-pub const CUBE_VERTICES: [Vertex; 8] = [
-    Vertex {position: [-1.0, 1.0, 0.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [-1.0, -1.0, 0.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [1.0, -1.0, 0.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [1.0, 1.0, 0.0], normal: [0.0, 0.0, 0.0]},
+/*
+let vertices = vec![
+    Vertex {position: [-1.0, -1.0, -1.0]},
+    Vertex {position: [-1.0, 1.0, -1.0]},
+    Vertex {position: [1.0, 1.0, -1.0]},
+    Vertex {position: [1.0, -1.0, -1.0]},
 
-    Vertex {position: [-1.0, 1.0, -1.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [-1.0, -1.0, -1.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [1.0, -1.0, -1.0], normal: [0.0, 0.0, 0.0]},
-    Vertex {position: [1.0, 1.0, -1.0], normal: [0.0, 0.0, 0.0]},
+    Vertex {position: [-1.0, -1.0, 1.0]},
+    Vertex {position: [-1.0, 1.0, 1.0]},
+    Vertex {position: [1.0, 1.0, 1.0]},
+    Vertex {position: [1.0, -1.0, 1.0]},
 ];
-
-pub struct Mesh {
-    vertices: Vec<Vertex>
-}
-
-impl Mesh {
-    pub fn load(vertices: Vec<Vertex>) -> Self {
-        Self {vertices}
-    }
-}
-
-pub struct Object {
-    mesh: Mesh,
-    transform: Transform,
-    scale: f64
-}
-
-impl Object {
-    pub fn new(mesh: Mesh, position: DVec3) -> Self {
-        let transform = Transform::from_translation(position);
-
-        Self {
-            mesh,
-            transform,
-            scale: 1.0
-        }
-    }
-
-    pub fn with_scale(mut self, scale: f64) -> Self {
-        self.scale = scale;
-        self
-    }
-}
+*/
