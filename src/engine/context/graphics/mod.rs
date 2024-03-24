90860c497259a63742b4e00fa6ee881178f77fa4
Delfi1 <fezdx@mail.ru>
2024-03-24 21:10:11 +0300

    Shaders test #3

--- src/engine/context/graphics/mod.rs
+++ src/engine/context/graphics/mod.rs
@@ -64,16 +64,62 @@ mod fs2d {
 
             layout(location=0) out vec4 fragColor;
 
+            vec3 palette(float d){
+                return mix(vec3(0.2,0.7,0.9),vec3(1.,0.,1.),d);
+            }
+
+            vec2 rotate(vec2 p,float a){
+                float c = cos(a);
+                float s = sin(a);
+                return p*mat2(c,s,-s,c);
+            }
+
+            float map(vec3 p){
+                for( int i = 0; i<8; ++i){
+                    float t = inp.time*0.2;
+                    p.xz =rotate(p.xz,t);
+                    p.xy =rotate(p.xy,t*1.89);
+                    p.xz = abs(p.xz);
+                    p.xz-=.5;
+                }
+                return dot(sign(p),p)/5.;
+            }
+
+            vec4 rm (vec3 ro, vec3 rd){
+                float t = 0.;
+                vec3 col = vec3(0.);
+                float d;
+                for(float i =0.; i<64.; i++){
+                    vec3 p = ro + rd*t;
+                    d = map(p)*.5;
+                    if(d<0.02){
+                        break;
+                    }
+                    if(d>100.){
+                        break;
+                    }
+                    //col+=vec3(0.6,0.8,0.8)/(400.*(d));
+                    col+=palette(length(p)*.1)/(400.*(d));
+                    t+=d;
+                }
+                return vec4(col,1./(d*100.));
+            }
             void main()
             {
-                // Normalized pixel coordinates (from 0 to 1)
-                vec2 uv = fragCoord / inp.resolution.xy;
+                vec2 uv = (fragCoord-(inp.resolution.xy/2.))/inp.resolution.x;
+                vec3 ro = vec3(0.,0.,-50.);
+                ro.xz = rotate(ro.xz,inp.time);
+                vec3 cf = normalize(-ro);
+                vec3 cs = normalize(cross(cf,vec3(0.,1.,0.)));
+                vec3 cu = normalize(cross(cf,cs));
 
-                // Time varying pixel color
-                vec3 col = 0.5 + 0.5*cos(inp.time + uv.xyx + vec3(0,2,4));
+                vec3 uuv = ro+cf*3. + uv.x*cs + uv.y*cu;
 
-                // Output to screen
-                fragColor = vec4(col,1.0);
+                vec3 rd = normalize(uuv-ro);
+
+                vec4 col = rm(ro,rd);
+
+                fragColor = col;
             }
         "#,
     }
